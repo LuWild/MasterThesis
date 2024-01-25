7b698ccbb9d3a4a0a723c7c8a4ace8cd9020a056
LuWild <Lukas.Wildberger@gmx.de>
2024-01-25 18:35:45 +0100

    implemented "calculate_function_value" methods for each class and a "get_used_gamma" for the class PiecewiseLinearArrivalCurve

--- arrival_curves.py
+++ arrival_curves.py
@@ -6,6 +6,18 @@ class TokenBucketArrivalCurve:
         self.rate = rate
         self.burst = burst
 
+    def calculate_function_value(self, t: float):
+        """
+        Calculates f(t) for the arrival curve object.
+
+        :param t: value t for which f(t) is calculated
+        :return: the calculated value f(t) for the given t
+        """
+        if t <= 0:
+            return 0
+        else:
+            return self.rate * t + self.burst
+
 
 class PiecewiseLinearArrivalCurve:
     def __init__(self, gammas: List[TokenBucketArrivalCurve]):
@@ -17,11 +29,50 @@ class PiecewiseLinearArrivalCurve:
         self.gammas = gammas
         self.intersections = self.__get_list_of_all_intersections()
 
+    def calculate_function_value(self, t: float):
+        """
+        Calculates f(t) for the arrival curve object.
+
+        :param t: value t for which f(t) is calculated
+        :return: the calculated value f(t) for the given t
+        """
+        import calculator
+        if t <= 0:
+            return 0
+        else:
+            return calculator.piecewise_linear_function(self, [t])[0]
+
+    def get_used_gamma(self, t: float):
+        """
+        Returns the TokenBucketArrivalCurve that is used to calculate f(t) in the segment for a given t
+
+        :param t: value t
+        :return: TokenBucketArrivalCurve for the segment of the given t
+        """
+        number_of_intersections = len(self.intersections)
+        for i in range(0, number_of_intersections):
+            if t <= self.intersections[i]:
+                return self.gammas[i]
+        return self.gammas[number_of_intersections]
+
+    def get_used_gamma_number(self, t: float):
+        """
+        Returns the number of the TokenBucketArrivalCurve that is used to calculate f(t) in the segment for a given t
+
+        :param t: value t
+        :return: the number of the TokenBucketArrivalCurve for the segment of the given t
+        """
+        gamma_used = self.get_used_gamma(t)
+        for i in range(0, len(self.gammas)):
+            if self.gammas[i].rate == gamma_used.rate and self.gammas[i].burst == gamma_used.burst:
+                return i + 1
+
+
     def __get_list_of_all_intersections(self):
         intersections = []
         for x in range(0, len(self.gammas) - 1):
             tb1 = self.gammas[x]
-            tb2 = self.gammas[x+1]
+            tb2 = self.gammas[x + 1]
             intersections.append((tb2.burst - tb1.burst) / (tb1.rate - tb2.rate))
 
         return intersections
--- services_curves.py
+++ services_curves.py
@@ -1,4 +1,16 @@
+import calculator
+
+
 class RateLatencyServiceCurve:
     def __init__(self, rate: float, latency: float):
         self.rate = rate
         self.latency = latency
+
+    def calculate_function_value(self, t: float):
+        """
+        Calculates f(t) for the service curve object.
+
+        :param t: value t for which f(t) is calculated
+        :return: the calculated value f(t) for the given t
+        """
+        return calculator.rate_latency_function(R=self.rate, T=self.latency, values=[t])[0]
