f8988b8149983a53022dec3bc5b3add4678d0a59
LuWild <Lukas.Wildberger@gmx.de>
2024-02-28 00:41:06 +0100

    implemented convolution for PWL AC + SC

--- dnc_operations/convolution.py
+++ dnc_operations/convolution.py
@@ -1,4 +1,5 @@
 from dnc_service.rate_latency_service_curve import RateLatencyServiceCurve
+from dnc_service.piecewise_linear_service_curve import PiecewiseLinearServiceCurve
 from dnc_arrivals.piecewise_linear_arrival_curve import PiecewiseLinearArrivalCurve
 
 
@@ -10,3 +11,11 @@ def convolution(arrival_curve: PiecewiseLinearArrivalCurve, service_curve: RateL
     else:
         return min(arrival_curve.calculate_function_value(t - T), service_curve.calculate_function_value(t))
 
+
+def convolution(arrival_curve: PiecewiseLinearArrivalCurve, service_curve: PiecewiseLinearServiceCurve, t: float):
+    T = service_curve.get_initial_latency()
+
+    if t <= T:
+        return 0
+    else:
+        return min(arrival_curve.calculate_function_value(t - T), service_curve.calculate_function_value(t))
